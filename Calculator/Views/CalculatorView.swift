0dcaee28c949f2fbc9ca7750f5fecb9e3d11d079
prv2 <jonathanliu2009@gmail.com>
2023-04-04 22:39:52 -0400

    created sidebar

--- Calculator/Views/CalculatorView.swift
+++ Calculator/Views/CalculatorView.swift
@@ -7,11 +7,24 @@
 
 import SwiftUI
 
+// Move this later
+extension UIApplication {
+    static var safeAreas: UIEdgeInsets  {
+        let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+        return scene?.windows.first?.safeAreaInsets ?? .zero
+    }
+}
+
 struct CalculatorView: View {
     @EnvironmentObject private var viewModel: CalculatorViewModel
 
     @State var value = "0"
     @State var currentNumber: Float = 0
+    
+    @State var edges = UIApplication.safeAreas
+    @State var screenWidth = UIScreen.main.bounds.width
+    
+    @State private var show = false
     
     var body: some View {
         NavigationStack {
@@ -19,6 +32,9 @@ struct CalculatorView: View {
                 settingsButton
                 VStack(alignment: .trailing) {
                     Spacer()
+                    // This text is the small gray text at the top
+                    // of the resultDisplay (replace it with a
+                    // separate view later
                     Text("102")
                         .foregroundColor(Color("TextColor"))
                         .font(.system(size: 30, weight: .medium))
@@ -30,6 +46,7 @@ struct CalculatorView: View {
                 }
                 .padding(K.padding)
                 
+                sideMenu
             }
         }
     }
@@ -43,11 +60,14 @@ struct CalculatorView_Previews: PreviewProvider {
 }
 
 extension CalculatorView {
+    
     private var settingsButton: some View {
         VStack {
             HStack {
                 Button(action: {
-                    
+                    withAnimation(.spring()) {
+                        show.toggle()
+                    }
                 }) {
                     VStack(alignment: .leading) {
                         /*
@@ -61,10 +81,10 @@ extension CalculatorView {
                             .frame(width: K.menuIconWidth, height: K.menuIconHeight)
                         Rectangle()
                             .fill(.white)
-                            .frame(width: 20, height: 3)
+                            .frame(width: K.menuIconWidth * 0.65, height: K.menuIconHeight)
                         Rectangle()
                             .fill(.white)
-                            .frame(width:K.menuIconWidth, height: 3)
+                            .frame(width: K.menuIconWidth * 0.4, height: K.menuIconHeight)
                     }
                     .padding(.leading, 18.0)
                 }
@@ -72,6 +92,50 @@ extension CalculatorView {
             }
             Spacer()
         }
+    }
+    
+    private var sideMenu: some View {
+        ZStack {
+            HStack(spacing: 0) {
+                VStack {
+                    HStack {
+                        // The X button
+                        Button(action: {
+                            withAnimation(.spring()) {
+                                show.toggle()
+                            }
+                        }, label: {
+                            Image(systemName: "xmark")
+                                .font(.system(size: 22, weight: .bold))
+                                .foregroundColor(Color.white.opacity(0.7))
+                        })
+                        
+                        Spacer(minLength: 0)
+                    }
+                    .padding()
+                    .padding(.top, edges.top)
+                    
+                    VStack(alignment: .leading, spacing: 0, content: {
+                        Text("Menu")
+                        SideMenuButton(title: "Settings", show: $show)
+                        SideMenuButton(title: "Contact", show: $show)
+                        SideMenuButton(title: "Fjakfe", show: $show)
+                    })
+                    .padding(.top)
+                    .padding(.leading, 0)
+                    
+                    Spacer(minLength: 0)
+                }
+                .frame(width: screenWidth * 0.8) // Later replace long thing with @State var screenWidth
+                .background(Color(red: 0.1, green: 0.1, blue: 0.1))
+                
+                Spacer(minLength: 0)
+            }
+            .background(Color.black.opacity(show ? 0.3 : 0))
+            .offset(x: show ? 0 : screenWidth - (screenWidth + (screenWidth * 0.8)))
+            
+        }
+        .ignoresSafeArea(.all, edges: .all)
     }
     
     private var resultDisplay: some View {
---
+++ Calculator/Views/Components/SideMenuButton.swift
@@ -0,0 +1,40 @@
+//
+//  SideMenuButtons.swift
+//  Calculator
+//
+//  Created by Jonathan Liu on 4/4/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension CalculatorView {
+    struct SideMenuButton: View {
+        var title: String
+        
+        @Binding var show: Bool
+        
+        var body: some View {
+            Button(action: {
+                withAnimation(.spring()) {
+                    show.toggle()
+                }
+            }, label: {
+                HStack(spacing: 0) {
+                    /*
+                    Image(systemName: image)
+                        .font(.system(size: 22))
+                        .frame(width: 25, height: 25)*/
+                    
+                    Text(title)
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                }
+                .padding(.vertical)
+                .padding(.trailing)
+            })
+            .padding(.top, 5)
+            .foregroundColor(Color.white.opacity(0.5))
+        }
+    }
+}
